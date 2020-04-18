48acb49b439b26f15d93e47934cb1b2f6963783c
汪大为 <wangdw@juwantech.cn>
2020-04-18 22:50:04 +0800

    选中操作

--- bms/native/js/service/AnnouncementService.js
+++ bms/native/js/service/AnnouncementService.js
@@ -6,10 +6,25 @@ var AnnouncementService = {
             var layUi_$ = layui.$, layer = layui.layer, form = layui.form, table = layui.table;
 
             //1.清楚上一次展示的数据,并设置新的视图。
-            globalService.setUIHtml("<table id=\"demoId\" class=\"layui-hide\" lay-filter=\"demoId\"></table>");
+            globalService.setUIHtml("            <div class=\"layui-btn-group ml-1 mt-2\">\n" +
+                "                <button type=\"button\" class=\"layui-btn layui-btn-primary layui-btn-sm\"><i class=\"layui-icon\"></i>\n" +
+                "                </button>\n" +
+                "                <button type=\"button\" class=\"layui-btn layui-btn-primary layui-btn-sm\"  lay-event=\"getCheckData\"><i class=\"layui-icon\"></i>\n" +
+                "                </button>\n" +
+                "                <button type=\"button\" class=\"layui-btn layui-btn-primary layui-btn-sm\"><i class=\"layui-icon\"></i>\n" +
+                "                </button>\n" +
+                "            </div>\n" +
+                "            <table id=\"demoId\" class=\"layui-hide\" lay-filter=\"demoId\"></table>\n" +
+                "\n" +
+                "\n" +
+                "            <script type=\"text/html\" id=\"toolbarDemo\">\n" +
+                "                <div class=\"layui-btn-container\">\n" +
+                "                    <button class=\"layui-btn layui-btn-sm\" lay-event=\"getCheckData\">获取选中行数据</button>\n" +
+                "                </div>\n" +
+                "            </script>");
             //2.发送ajax.
             $.ajax({
-                headers: {"X-Authentication-Token": globalService.tokenOfHeader },
+                headers: {"X-Authentication-Token": globalService.tokenOfHeader},
                 url: globalService.basePath + '/announcement',
                 type: "get",
                 contentType: "application/json",
@@ -21,7 +36,10 @@ var AnnouncementService = {
                         table.render({
                             elem: '#demoId'//绑定元素
                             , cellMinWidth: 80 //全局定义常规单元格的最小宽度，layui 2.2.1 新增
+                            ,toolbar: '#toolbarDemo'
                             , cols: [[
+                                {type: 'checkbox', width: 70},
+                                //{type:'radio'},
                                 {field: 'id', width: 70, title: 'ID', show: false, sort: true}
                                 , {field: 'title', width: 280, title: '标题'}
                                 , {field: 'content', title: '内容'}
@@ -30,6 +48,8 @@ var AnnouncementService = {
                             , even: true
                             , page: true //是否显示分页
                             , data: res.data
+                            ,height:'full-200'
+                            ,limit:30
                         });
                     } else {
                         layer.msg(res.msg);
@@ -42,11 +62,24 @@ var AnnouncementService = {
             });
 
             //绑定行点击事件。
-            table.on('row(demoId)', AnnouncementService.lookOne);
+            //   table.on('row(demoId)', AnnouncementService.lookOne);
+                table.on('toolbar(demoId)', function(obj){
+
+                    console.log(obj)
+
+                    // 行事件
+                    var checkStatus = table.checkStatus(obj.config.id); //获取选中行状态
+                    switch(obj.event){
+                        case 'getCheckData':
+                            var data = checkStatus.data;  //获取选中行数据
+                            layer.alert(JSON.stringify(data));
+                            break;
+                    };
+                });
 
         })
     },
-    lookOne:function (obj) {
+    lookOne: function (obj) {
         let lineData = obj.data;
 
         // for( field in lineData){
@@ -55,10 +88,9 @@ var AnnouncementService = {
 
         layer.open({
             title: lineData.title
-            ,content: lineData.content
-            ,area: ['1200px', '550px']
+            , content: lineData.content
+            , area: ['1200px', '550px']
         });
-
 
 
     },
